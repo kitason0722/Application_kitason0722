24715a0ffea86af68c1169fa479a844f69d7794e
kitason0722 <riku.kitamura.3g@stu.hosei.ac.jp>
2025-01-14 16:04:40 +0900

    ゲームの基本構造の構築
    とりあえず反応速度を測るゲーム（仮）

--- kitason0722/lib/main.dart
+++ kitason0722/lib/main.dart
@@ -1,71 +1,152 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ReactionGame());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ReactionGame extends StatelessWidget {
+  const ReactionGame({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'リアクションテストゲーム',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const GameScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _GameScreenState createState() => _GameScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GameScreenState extends State<GameScreen> {
+  final Random _random = Random();
+  Color _backgroundColor = Colors.white;
+  late Timer _timer;
+  bool _isTargetColor = false;
+  int _score = 0;
+  bool _gameRunning = false;
 
-  void _incrementCounter() {
-    setState(()
-    {
-      _counter++;
+  // ターゲットの色
+  final Color _targetColor = Colors.red;
+
+  // ゲームを開始する
+  void _startGame() {
+    setState(() {
+      _score = 0;
+      _gameRunning = true;
+    });
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        // 背景色をランダムに変更
+        _backgroundColor = Color.fromARGB(
+          255,
+          _random.nextInt(256),
+          _random.nextInt(256),
+          _random.nextInt(256),
+        );
+        // ターゲットの色かどうかを判定
+        _isTargetColor = _backgroundColor == _targetColor;
+      });
     });
   }
 
+  // ゲームを終了する
+  void _endGame() {
+    _timer.cancel();
+    setState(() {
+      _gameRunning = false;
+      _backgroundColor = Colors.white;
+    });
+  }
+
+  // タップ時の動作
+  void _onTap() {
+    if (_isTargetColor) {
+      setState(() {
+        _score++;
+      });
+    }
+  }
+
   @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold
+      (
+      appBar: AppBar
+        (
+        title: const Text('リアクションテストゲーム'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: GestureDetector
+        (
+        onTap: _gameRunning ? _onTap : null,
+        child: Container
+          (
+          color: _backgroundColor,
+          child: Center
+            (
+            child: _gameRunning
+                ? Column
+              (
+              mainAxisSize: MainAxisSize.min,
+              children:
+              [
+                Text
+                  (
+                  'スコア: $_score',
+                  style: const TextStyle(fontSize: 32), // スコアのフォントサイズを調整
+                ),
+                const SizedBox(height: 20),
+                const Text
+                  (
+                  '背景が赤になったら押せ!',
+                  style: TextStyle(fontSize: 20,
+                      fontWeight: FontWeight.bold), // メッセージのフォントサイズを調整
+                ),
+              ],
+            )
+                : SizedBox
+              (
+              width: 200, // ボタンの幅
+              child: ElevatedButton
+                (
+                onPressed: _startGame,
+                style: ElevatedButton.styleFrom
+                  (
+                  minimumSize: const Size(200, 60), // ボタンサイズを指定
+                ),
+                child: const Text
+                  (
+                  'スタート',
+                  style: TextStyle(fontSize: 20), // ボタン文字サイズ
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: _gameRunning
+          ? FloatingActionButton(
+        onPressed: _endGame,
+        child: const Icon(Icons.stop),
+      )
+          : null,
     );
   }
 }
